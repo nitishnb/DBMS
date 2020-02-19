c0ac7975c8b40abefd09072253e2c7aac3d6b3a8
Nitish N Banakar <53825720+nitishnb@users.noreply.github.com>
2020-02-19 15:46:24 +0530

    Create order processing database.sql

---
+++ order processing database.sql
@@ -0,0 +1,58 @@
+CREATE TABLE CUSTOMER1 (
+Custno number(10),
+cname varchar2(30),
+city varchar2(30),
+primary key (Custno)
+);
+
+
+CREATE TABLE ORDER1 (
+Orderno number(10),
+Odate date,
+Custno number(10),
+Orderamount number(10,2),
+primary key (Orderno),
+foreign key(Custno) references CUSTOMER1(Custno)
+);
+
+CREATE TABLE ITEM1(
+Itemno number(10),
+Price number(10,2),
+primary key (Itemno)
+);
+
+CREATE TABLE ORDERITEM (
+Orderno number(10),
+Itemno number(10),
+Qty number(3),
+foreign key(Orderno) references ORDER1(Orderno),
+foreign key(Itemno) references ITEM1(Itemno)
+);
+
+CREATE TABLE WAREHOUSE(
+Warehouseno NUMBER(10),
+City VARCHAR2(20),
+primary key (Warehouseno)
+);
+
+CREATE TABLE SHIPMENT( 
+Orderno NUMBER(10),
+Warehouseno NUMBER(10),
+Shipment_Date DATE,
+foreign key(Orderno) references ORDER1(Orderno),
+foreign key(Warehouseno) references WAREHOUSE(Warehouseno)
+);
+
+INSERT INTO CUSTOMER1 VALUES('&Custno','&cname','&city');
+INSERT INTO ORDER1 VALUES('&Orderno','&Odate','&Custno','&Orderamount');
+INSERT INTO ITEM1 VALUES('&Itemno','&Price');
+INSERT INTO ORDERITEM VALUES('&Orderno','&Itemno','&Qty11');
+INSERT INTO WAREHOUSE VALUES('&Warehouseno','&City');
+INSERT INTO SHIPMENT VALUES('&Orderno','&Warehouseno','&Shipment_Date');
+
+SELECT * FROM CUSTOMER1;
+SELECT * FROM ORDER1;
+SELECT * FROM ITEM1;
+SELECT * FROM ORDERITEM;
+SELECT * FROM WAREHOUSE;
+SELECT * FROM SHIPMENT;
